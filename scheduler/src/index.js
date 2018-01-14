7f24d80bb0e7357ad4da4136ed175c4ce1669f6e
lu wang <Demon@lus-MacBook-Pro.local>
2018-01-13 22:39:27 -0500

    basic setup

--- scheduler/src/index.js
+++ scheduler/src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class TimeTable extends React.Component {
+
+	
+	CreateColumn(){
+		for (var i = 0; i < 7; i++) {
+			React.createElement("tr")
+		}
+	}
+	render() {
+		return (
+			React.createElement("div", {id: "TimeTableDIV"},
+				React.createElement("table", {id: "TimeTable"},
+					this.CreateColumn()
+				)
+			)
+		);
+	}
+}
+
+// ========================================
+
+ReactDOM.render(
+  <TimeTable />,
+  document.getElementById('root')
+);
