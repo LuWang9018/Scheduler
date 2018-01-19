6e703090eb2f391580a324b8c5f492c3f2b2333f
Lu <Canada_wl@aliyun.com>
2018-01-18 22:56:05 -0500

    add webpack and babel

--- scheduler/src/js/index.js
+++ scheduler/src/js/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {TableGen} from './timeTable/cellGen.js';
-import './index.css';
+import '../css/index.css';
 
+import test from '../assets/try.jpg';
 // ========================================
 /*
 Add user setting later
@@ -14,10 +15,13 @@ function GenUI(){
 
 
 ReactDOM.render(
-  <TableGen 
-	//TODO
-	//change Time format later!
-  	TimeRange={[6, 11]}
-  />,
+	<div>
+		<TableGen 
+		//TODO
+		//change Time format later!
+			TimeRange={[6, 11]}
+		/>
+		<img src = {test} alt='test'/>
+	</div>,	
   document.getElementById('root')
 );
